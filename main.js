5fe56e4ef968db27b428195b2f143a308c5f6b90
Bahman Soltani <bahman.soltani@gmail.com>
2023-01-17 13:16:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,41 @@ menuclicked = function (s) {
 //        e.addClass("typing");
 //})
 
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+if(checkCookie){
+    $("#langDe").hide();
+    $("#langEn").show();
+    setCookie('lang', 'EN', 1);
+}
+
+function checkCookie() {
+  let l = getCookie("lang");
+    if (l != "" && l != null && l != undefined) {
+      return true;
+    else
+      return false;
+}
 
 changelange = function () {
     if(!$.cookie('lang')) {
