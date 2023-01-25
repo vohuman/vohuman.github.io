b6b9d5eeb3de727e0c43449dab472529bce7e7e4
Bahman Soltani <bahman.soltani@gmail.com>
2023-01-25 13:56:27 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+var app = angular.module('langApp', []);
+
+app.service('PageService', function($window) {
+       return {
+          setTitle: function (newTitle) { $window.document.title = newTitle; }
+       };
+   });
+
+app.controller('langCtrl', [
+               '$scope', '$location', 'PageService',
+               function ($scope, $location, PageService) {
+
+                    $scope.lang = false;
+
+                    $scope.title = {about: 'Über mich | Bahman Soltani',
+                        tech: 'Technische Erfahrungen | Bahman Soltani',
+                        proj: 'Projekte & Zertifikate | Bahman Soltani',
+                        cont: 'Kontakt | Bahman Soltani'};
+
+                    $scope.chooseLang = [{about: 'About me | Bahman Soltani',
+                    tech: 'Technical Experiences | Bahman Soltani',
+                    proj: 'Projects & Certificates | Bahman Soltani',
+                    cont: 'Contact | Bahman Soltani'},
+                    {about: 'Über mich | Bahman Soltani',
+                    tech: 'Technische Erfahrungen | Bahman Soltani',
+                    proj: 'Projekte & Zertifikate | Bahman Soltani',
+                    cont: 'Kontakt | Bahman Soltani'}];
+
+                    changel = function (l1) {
+
+                        $scope.lang = !l1;
+
+                        if(!($scope.lang))
+                            $scope.title = $scope.chooseLang[0];
+                        else
+                            $scope.title = $scope.chooseLang[1];
+
+                        PageService.setTitle($scope.title.about);
+                    };
+
+                    this.setPageTitle = PageService.setTitle;
+
+
+                    changel($scope.lang);
+
+                    console.log($location.absUrl());
+
+                    window.document.title=$scope.title.about;
+
+
+               }
+]);
