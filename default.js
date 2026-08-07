9059b15561df6ccd3bf24287b94b90ec00f77ac8
Bahman Soltani <bahman.soltani@gmail.com>
2026-08-07 17:21:29 +0200

    Remove text-primary class from links

--- default.js
+++ default.js
@@ -528,7 +528,7 @@ loadedu = function () {
           <div>
               <h6 class="text-success mb-0">${e.degree}</h6>
               <p class="text-success small fw-medium mb-1">
-                  <a href="${e.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none link1 text-primary">
+                  <a href="${e.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none link1">
                     ${e.institution}
                   </a>
               </p>
@@ -561,7 +561,7 @@ loadedu = function () {
         html += `<div class="cer">
           <i class="fa-solid fa-trophy text-orange mt-1 fs-5"></i>
           <div style="padding-left: 1rem;">
-              <h6 class="text-dark mb-1 small">
+              <h6 class="mb-1 small">
                    <a href="${c.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none link1 text-primary">
                        <b>${c.title}</b>
                    </a>
