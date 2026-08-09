d5cf64d799bfee8a4d0a171181ff670263b32058
Bahman Soltani <bahman.soltani@gmail.com>
2026-08-09 21:37:02 +0200

    Update link references for role and company

--- default.js
+++ default.js
@@ -310,7 +310,7 @@ loadhistory = function () {
       <div>
           <h5 class="fw-bold text-dark mb-1">${h.role}</h5>
           <div class="d-flex flex-wrap align-items-center gap-2 text-secondary small">
-              <a href="${c.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none link1">
+              <a href="${h.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none link1">
                        <span class="fw-semibold">
               <i class="fa-regular fa-building me-1"></i>
               ${h.company}</span>
@@ -344,7 +344,7 @@ loadhistory = function () {
               <div>
                   <h5 class="fw-bold text-dark mb-1">${h.role}</h5>
                   <div class="d-flex flex-wrap align-items-center gap-2 text-secondary small">
-                     <a href="${c.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none link1">
+                     <a href="${h.link}" target="_blank" rel="noopener noreferrer" class="text-decoration-none link1">
                        <span class="fw-semibold">
               <i class="fa-regular fa-building me-1"></i>
               ${h.company}</span>
